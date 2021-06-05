093e383212a6eda67ebc8340b1a2379a2ccde436
Lautarocm <lcardenasmancini@gmail.com>
2021-06-05 18:02:23 -0300

    cree log-in usando ciclos

---
+++ js/js.js
@@ -0,0 +1,25 @@
+let username = "";
+let password = "";
+let loginUsername = "";
+let loginPassword = "";
+
+while(username == ""){
+    username = prompt("Crea un usuario")
+}
+while(password == ""){
+    password = prompt("Crea una contraseña")
+}
+while(loginUsername == ""){
+    loginUsername = prompt("Ingresa tu usuario")
+}
+while(loginPassword == ""){
+    loginPassword = prompt("Ingresa tu contraseña")
+}
+
+if((username === loginUsername) && (password === loginPassword)){
+    alert(succesLogin)
+    alert("Bienvenido")
+}
+else{
+    alert("Usuario /y contraseña incorrectos")
+}
